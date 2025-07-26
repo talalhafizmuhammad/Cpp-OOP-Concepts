494b95bd17b180842dc547b5d0d66dad5c640fc7
Hafiz Muhammad Talal <muhammadtala20201@gmail.com>
2025-07-27 04:30:37 +0500

    Create 03. Enumeration(enums).cpp

---
+++ 03. Enumeration(enums).cpp
@@ -0,0 +1,13 @@
+// Enum: define a set of named integer constants
+#include <iostream>
+using namespace std;
+
+enum Meal { breakfast, lunch, dinner }; // breakfast = 0, lunch = 1, dinner = 2
+
+int main() {
+    Meal m1 = breakfast;
+
+    cout << "Enum value of m1 (breakfast): " << m1 << endl;
+
+    return 0;
+}
