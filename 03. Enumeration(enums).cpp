6da4834fd7e222545be91464bc9a0d050ee9a882
Hafiz Muhammad Talal <muhammadtala20201@gmail.com>
2025-07-29 04:41:28 +0500

    Update 03. Enumeration(enums).cpp
    Enumeration for assigning number to specific value

--- 03. Enumeration(enums).cpp
+++ 03. Enumeration(enums).cpp
@@ -5,7 +5,7 @@ using namespace std;
 enum Meal { breakfast, lunch, dinner }; // breakfast = 0, lunch = 1, dinner = 2
 
 int main() {
-    Meal m1 = breakfast;
+    Meal m1 = breakfast; //accessing the element of enum
 
     cout << "Enum value of m1 (breakfast): " << m1 << endl;
 
